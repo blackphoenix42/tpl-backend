de5325c97a29f24d6b10f1212a2fa5adbbb371fe
Ayush Yadav <aayush.sang@gmail.com>
2021-10-23 12:21:02 +0530

    update

--- PinataKeys.js
+++ PinataKeys.js
@@ -3,3 +3,7 @@ module.exports = {
     API_KEY: process.env.API_KEY,
     API_SECRET: process.env.API_SECRET
 }
+// Player Contract - KT1QnSatgdCUPbAkAfAUkRz6JZf97AgUw5vi
+// Game Contract - KT1UoQGriASpkVnY9N9nCTMKUrESCUd3Ucvg
+// PLAY Token Contract - KT1HnJ8RrPLKkRXzkxDfYXD28RgsC2n63BcR
+// NFT Contract - KT1UULKxZrkhuUkTxfiJgB2zKwHQe8WMhLoh
